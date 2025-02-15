1229a8904a63b7f6cdde1c9472acc9112976a088
Deepesh Dandriyal <96759552+DeepeshDandriyal@users.noreply.github.com>
2025-02-15 08:20:14 +0000

    convertion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+// let score=null
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+const isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
