a02b83e25d2fb69c0d85b907c8ffec35db0a56a8
Deepesh Dandriyal <96759552+DeepeshDandriyal@users.noreply.github.com>
2025-02-15 07:23:58 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+
+let accountEmail="deepes@gmail.com"
+
+var accountPassword="12345"
+
+accountCity="dehradun"
+
+
+
+// accountId=2 not allowed as it is declared as const
+
+accountEmail="hc@gmail.com"
+accountPassword="23454"
+accountCity="Pune"
+
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js code as newer version
+
+// alert("hello") we are using nodejs not browser
+
+let name="deepesh"
+let age=27
+let isLoggedIn=true
+let state=null
+
+//number
+//string
+//boolean
+//null=>standalone value
+//undefined=>abhi value assign nhi hui
+//symbol=>unique
+
+
+
+//object
+
+
+console.log(typeof(null))
